35106ed0d497f00ba2283fc0206dd64caf5236fa
roqytis <91776641+roqytis@users.noreply.github.com>
2021-10-26 00:48:32 +0900

    03_Servlet2_init_param

---
+++ 03_Servlet2_init_param/src/com/controller/OtherServlet.java
@@ -0,0 +1,34 @@
+package com.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class OtherServlet
+ */
+@WebServlet("/OtherServlet")
+public class OtherServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    public OtherServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("OtherServlet.doGet()=============");
+		String userid=getInitParameter("userid");
+		String email=getInitParameter("email");
+		System.out.println(userid+"\t"+email);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
