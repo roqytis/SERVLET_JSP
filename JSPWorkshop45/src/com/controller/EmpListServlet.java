c6511bc96ba547ce179dc22bb985a34335245883
roqytis <91776641+roqytis@users.noreply.github.com>
2021-10-26 01:16:46 +0900

    JSPWorkshop45

---
+++ JSPWorkshop45/src/com/controller/EmpListServlet.java
@@ -0,0 +1,42 @@
+package com.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dao.EmpDAO;
+import com.dto.EmpDTO;
+import com.service.EmpService;
+
+/**
+ * Servlet implementation class EmpListServlet
+ */
+@WebServlet("/EmpListServlet")
+public class EmpListServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		String searchName = request.getParameter("searchName");
+		String searchValue = request.getParameter("searchValue");
+		System.out.println(searchName+"\t"+searchValue);////
+		EmpService service = new EmpService();
+		//List<EmpDTO> list = service.select();//전체검색
+		
+		List<EmpDTO> list = service.select(searchName, searchValue);//검색분야, 검색어 넘김 
+		
+		request.setAttribute("list", list);
+		//forward
+		RequestDispatcher dis = request.getRequestDispatcher("list.jsp");
+		dis.forward(request, response);
+		
+	}
+
+}
