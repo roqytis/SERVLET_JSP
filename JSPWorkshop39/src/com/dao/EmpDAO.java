83923f94b9fd2b5aa79099b9d80353a3e1ed2fee
roqytis <91776641+roqytis@users.noreply.github.com>
2021-10-26 01:13:21 +0900

    JSPWorkshop39

---
+++ JSPWorkshop39/src/com/dao/EmpDAO.java
@@ -0,0 +1,20 @@
+package com.dao;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import org.apache.ibatis.session.SqlSession;
+import com.dto.EmpDTO;
+
+public class EmpDAO {
+	
+
+	public List<EmpDTO> select(SqlSession session) {
+		List<EmpDTO> list =  session.selectList("com.dto.selectAll");
+		return list;
+	}
+	
+	
+	
+	
+}
---
+++ JSPWorkshop39/src/com/service/EmpService.java
@@ -0,0 +1,33 @@
+package com.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.config.MySqlSessionFactory;
+import com.dao.EmpDAO;
+import com.dto.EmpDTO;
+
+public class EmpService {
+	
+
+	public List<EmpDTO> select() {
+	 EmpDAO dao = new   EmpDAO();
+	 SqlSession session = MySqlSessionFactory.getSession();
+	 List<EmpDTO> list = null; 
+	 try {
+      list = dao.select(session);
+    	  
+//      }catch(Exception e) {
+//    	  e.printStackTrace();
+      }finally {
+		session.close();
+      }
+      return list;
+	}//end select
+	
+	
+	
+}
