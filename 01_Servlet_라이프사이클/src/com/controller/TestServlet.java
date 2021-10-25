56d00b70d4d309d8e523cee6ba6f065fe743ed6c
roqytis <91776641+roqytis@users.noreply.github.com>
2021-10-26 00:43:14 +0900

    01_Servlet_라이프사이클

---
+++ 01_Servlet_라이프사이클/src/com/controller/TestServlet.java
@@ -0,0 +1,41 @@
+package com.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
+
+          
+    @Override
+	public void destroy() {
+		System.out.println("destory 매서드 호출");
+	}
+
+	@Override
+	public void init() throws ServletException {
+		System.out.println("init 매서드 호출");
+	}
+
+	public TestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("doget 호출");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		System.out.println("dopost 호출");
+	}
+
+}
